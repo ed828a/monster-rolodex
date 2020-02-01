ff0602e7f859e1c183ec31124dc9793fd5fe75f4
ed828a <ed828a@gmail.com>
2020-02-01 19:11:46 +1030

    Updates

---
+++ precache-manifest.fc137aa8c30b4c1583a6644e06c5a298.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43395f9a11f8f0a9919a96edc9a33158",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "c611462559353e758390",
+    "url": "/monster-rolodex/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "601bfb06af71cdf1d3f9",
+    "url": "/monster-rolodex/static/css/main.bab8cf34.chunk.css"
+  },
+  {
+    "revision": "c611462559353e758390",
+    "url": "/monster-rolodex/static/js/2.1e3d3700.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monster-rolodex/static/js/2.1e3d3700.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "601bfb06af71cdf1d3f9",
+    "url": "/monster-rolodex/static/js/main.2cde9ef9.chunk.js"
+  },
+  {
+    "revision": "bc09d86fba44e1c7ba83",
+    "url": "/monster-rolodex/static/js/runtime-main.8000f3b5.js"
+  }
+]);
